63ec763ddd8953f3709e619597e6588bf2ec5d6b
Jeff Thomassen <jeff.thomassen@ucdenver.edu>
2021-07-20 17:38:10 -0600

    commit tree 6693bf19e26ee1a1f82ec7b2ead58934dea2469f

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+function saturdayFun(action = 'roller-skate'){
+    return `This Saturday, I want to ${action}!`
+}
+
+const mondayWork = function(action = 'go to the office'){
+    return `This Monday, I will ${action}.`
+}
+
+function wrapAdjective(flair = '*'){
+    return function(adjective = 'special'){
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+
+const promptFunction = wrapAdjective('&')('a sad man')
